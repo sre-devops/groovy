d796d0effb2df9dbc986760ef5ab2695acb679a9
ihor.k <ihor.k@twsgo.com>
2021-01-23 17:03:35 +0200

    owasp

--- vars/jenkins.groovy
+++ vars/jenkins.groovy
@@ -1,9 +1,13 @@
-timestamps {
-    ansiColor('xterm') {
-        node('master') {
-            stage('Git CheckOut') {
-                owasp()
+def call() {
+    timestamps {
+        ansiColor('xterm') {
+            node('master') {
+                stage('Git CheckOut') {
+                    git credentialsId: '2fc867d7-eaa8-45ff-95e7-3262148e4f23', url: 'https://github.com/fhause5/petlinic-fe-original.git'
+                    sh 'npm install'
+                    owasp()
 
+                }
             }
         }
     }
