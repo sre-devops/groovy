85f904d59c9417162b72cb48f044d5825c266f5a
ihor.k <ihor.k@twsgo.com>
2021-01-23 15:08:06 +0200

    owasp

--- vars/owasp.groovy
+++ vars/owasp.groovy
@@ -1,6 +1,6 @@
 def call() {
     try {
-        dependencyCheck additionalArguments: '--scan $WORKSPACE --format HTML --enableExperimental', odcInstallation: 'Dependency Checkers'
+        dependencyCheck additionalArguments: '--scan . --format HTML --enableExperimental', odcInstallation: 'Dependency Checkers'
         dependencyCheckPublisher pattern: 'dependency-check-report.xml'
     } catch(Exception e) {
         println('[ERROR] The dependencyCheck does not work')
