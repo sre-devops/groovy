ffe23746753cdfb5f6feb0134445f74213411fb1
devops <devops>
2021-01-25 16:42:36 +0200

    alpine

--- vars/jenkins.groovy
+++ vars/jenkins.groovy
@@ -3,10 +3,7 @@ def call() {
         ansiColor('xterm') {
             node('master') {
                 stage('Git CheckOut') {
-                    git credentialsId: '2fc867d7-eaa8-45ff-95e7-3262148e4f23', url: 'https://github.com/fhause5/petlinic-fe-original.git'
-                    sh 'npm install'
-                    owasp()
-
+                    aqua("alpine")
                 }
             }
         }
