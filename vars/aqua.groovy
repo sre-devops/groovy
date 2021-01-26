9215691f998e75e1e3353f8e76959bdbc47c88d2
devops <devops>
2021-01-26 09:40:28 +0200

    alpine

--- vars/aqua.groovy
+++ vars/aqua.groovy
@@ -1,6 +1,8 @@
-def call(image) {
+def call(images) {
     try {
-        aquaMicroscanner imageName: '${image}', notCompliesCmd: '', onDisallowed: 'ignore', outputFormat: 'html'
+        images.each { item ->
+            aquaMicroscanner imageName: item, notCompliesCmd: '', onDisallowed: 'ignore', outputFormat: 'html'
+        }
     } catch(Exception e) {
         println('[ERROR] The Aqua Microscanner does not work')
     }
--- vars/jenkins.groovy
+++ vars/jenkins.groovy
@@ -1,11 +1,32 @@
+
 def call() {
+
+    properties([
+            buildDiscarder(
+                    logRotator(
+                            artifactDaysToKeepStr: '90',
+                            artifactNumToKeepStr: '20',
+                            daysToKeepStr: '90',
+                            numToKeepStr: '20'
+                    )
+            ),
+            parameters([
+                    string(
+                            "defaultValue": "nginx:latest, alpine:latest",
+                            "description": "Comma separated user's emails. \nE.g.: ivan_ivanov@epam.com, john_doe@epam.com",
+                            "name": "IMAGES"
+                    )
+            ])
+    ])
     timestamps {
         ansiColor('xterm') {
             node('master') {
                 stage('Git CheckOut') {
-                    aqua("alpine")
+                    images = params.IMAGES
+                    aqua(images)
                 }
             }
         }
     }
+
 }
