dc6d16b34dfa996c866590eb2dbc5ff08e43ee0b
devops <devops>
2021-01-26 17:44:10 +0200

    alpinegh56

--- vars/aqua.groovy
+++ vars/aqua.groovy
@@ -1,8 +1,11 @@
 def call(images) {
     try {
-        images.each { item ->
-            aquaMicroscanner imageName: item, notCompliesCmd: '', onDisallowed: 'ignore', outputFormat: 'html'
-        }
+//        images.each { item ->
+//            aquaMicroscanner imageName: item, notCompliesCmd: '', onDisallowed: 'ignore', outputFormat: 'html'
+//        }
+        docker.build("aqua-check:${BUILD_NUMBER}",'--no-cache -f Dockerfile .')
+        aquaMicroscanner imageName: item, notCompliesCmd: '', onDisallowed: 'ignore', outputFormat: 'html'
+        sh "docker rmi aqua-check:${BUILD_NUMBER}"
     } catch(Exception e) {
         println('[ERROR] The Aqua Microscanner does not work')
     }
--- vars/jenkins.groovy
+++ vars/jenkins.groovy
@@ -1,55 +1,39 @@
-def call() {
-    properties([
-            buildDiscarder(
-                    logRotator(
-                            artifactDaysToKeepStr: '90',
-                            artifactNumToKeepStr: '20',
-                            daysToKeepStr: '90',
-                            numToKeepStr: '20'
-                    )
-            ),
-            parameters([
-                    string(
-                            "defaultValue": "",
-                            "description": "Input Git url",
-                            "name": "Git_url"
-                    ),
-                    string(
-                            "defaultValue": "2fc867d7-eaa8-45ff-95e7-3262148e4f23",
-                            "description": "Input Jenkins credentials",
-                            "name": "Credentials"
-                    ),
-                    string(
-                            "defaultValue": "nginx:latest, alpine:latest",
-                            "description": "Comma separated images. \nE.g.: nginx:latest, alpine:latest",
-                            "name": "IMAGES"
-                    )
-            ])
-    ])
-    timestamps {
-        ansiColor('xterm') {
-            node('master') {
-                stage('OWASP security check') {
-                    if (params.Git_url.isEmpty())
-                        error("[ERROR] Git_url  can not be empty!")
-                    git_url = params.Git_url
-                    credentials = params.Credentials
+@Library('github.com/sre-devops/groovy') _
 
-                    println("[DEBUG] git_url: ${git_url}")
-                    println("[DEBUG] credentials: ${credentials}")
-
-                    git credentialsId: "${credentials}", url: "${git_url}"
-                    sh 'npm install'
-                    owasp()
-                }
-                stage('Aqua images security check') {
-                    if (params.IMAGES.isEmpty())
-                        error("[ERROR] IMAGES list can not be empty!")
-
-                    images = params.IMAGES.replaceAll(" ", "").toLowerCase().split(',')
-                    println("[DEBUG] images: ${images}")
-                    aqua(images)
-                }
+properties([
+        buildDiscarder(
+                logRotator(
+                        artifactDaysToKeepStr: '90',
+                        artifactNumToKeepStr: '20',
+                        daysToKeepStr: '90',
+                        numToKeepStr: '20'
+                )
+        ),
+        parameters([
+                string(
+                        "defaultValue": "https://github.com/fhause5/petlinic-fe-original.git",
+                        "description": "Input Git url",
+                        "name": "Git_url"
+                ),
+                string(
+                        "defaultValue": "2fc867d7-eaa8-45ff-95e7-3262148e4f23",
+                        "description": "Input Jenkins credentials",
+                        "name": "Credentials"
+                )
+        ])
+])
+timestamps {
+    ansiColor('xterm') {
+        node('master') {
+            stage('OWASP security check') {
+                if (params.Git_url.isEmpty())
+                    error("[ERROR] Git_url  can not be empty!")
+                git credentialsId: "${params.Credentials}", url: "${Git_url}"
+                sh 'npm install'
+                owasp()
+            }
+            stage('Aqua images security check') {
+                aqua(images)
             }
         }
     }
