ff40b64e03e1fe711f774526d5570121a3b85e05
devops <devops>
2021-01-27 11:01:49 +0200

    aqua-check

--- vars/owasp.groovy
+++ vars/owasp.groovy
@@ -1,6 +1,5 @@
 def call() {
     try {
-        sh 'npm install || true'
         dependencyCheck additionalArguments: '--scan . --format XML --enableExperimental', odcInstallation: 'Dependency Checkers'
         dependencyCheckPublisher pattern: 'dependency-check-report.xml'
     } catch(Exception e) {
